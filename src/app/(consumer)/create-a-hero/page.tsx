b86d1972a788d04d81e35170a451e0a4ecbd3d23
Weaver17 <ajweaver18@gmail.com>
2025-04-25 19:37:59 -0400

    fix: c-a-h fix?

--- src/app/(consumer)/create-a-hero/page.tsx
+++ src/app/(consumer)/create-a-hero/page.tsx
@@ -1,7 +1,9 @@
+import Loading from "@/app/loading/loading";
 import HeroForm from "@/components/hero-form/hero-form";
 import { prisma } from "@/lib/prisma";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
 async function page() {
   const { isAuthenticated, getUser } = getKindeServerSession();
@@ -32,28 +34,30 @@ async function page() {
   const userId = userRecord?.id;
 
   return (
-    <div className="py-6 px-2 bg-[url('../../public/random-dark.jpg')] bg-cover bg-no-repeat bg-center min-h-screen">
-      <div className="flex flex-col gap-6 mx-auto mb-6 justify-center items-center max-w-[70%]">
-        <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl lg:text-6xl">
-          Create-A-Hero
-        </h2>
-        <p className=" text-center md:font-semibold md:text-lg lg:text-xl">
-          Customize your hero and click submit to save the hero to your profile.
-          Your hero will then show up on the Custom Hero list and get their own
-          Hero Page
-        </p>
-        <p className=" text-center text-sm md:text-base md:font-semibold ">
-          All fields are required
-        </p>
+    <Suspense fallback={<Loading />}>
+      <div className="py-6 px-2 bg-[url('../../public/random-dark.jpg')] bg-cover bg-no-repeat bg-center min-h-screen">
+        <div className="flex flex-col gap-6 mx-auto mb-6 justify-center items-center max-w-[70%]">
+          <h2 className="font-semibold text-xl sm:text-2xl md:text-4xl lg:text-6xl">
+            Create-A-Hero
+          </h2>
+          <p className=" text-center md:font-semibold md:text-lg lg:text-xl">
+            Customize your hero and click submit to save the hero to your
+            profile. Your hero will then show up on the Custom Hero list and get
+            their own Hero Page
+          </p>
+          <p className=" text-center text-sm md:text-base md:font-semibold ">
+            All fields are required
+          </p>
+        </div>
+        <div className="section border border-secondary rounded-2xl bg-background/40 backdrop-blur-sm">
+          <HeroForm
+            kindeId={kindeId ?? ""}
+            username={username ?? ""}
+            userId={userId ?? ""}
+          />
+        </div>
       </div>
-      <div className="section border border-secondary rounded-2xl bg-background/40 backdrop-blur-sm">
-        <HeroForm
-          kindeId={kindeId ?? ""}
-          username={username ?? ""}
-          userId={userId ?? ""}
-        />
-      </div>
-    </div>
+    </Suspense>
   );
 }
 
